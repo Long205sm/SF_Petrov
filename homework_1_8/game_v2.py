25b247526bbf9026d39945b1cbb3d4b91d26fdcf
Long205sm <141716837+Long205sm@users.noreply.github.com>
2023-09-21 11:46:28 +0300

    PY_13 theory

--- homework_1_8/game_v2.py
+++ homework_1_8/game_v2.py
@@ -16,9 +16,16 @@ def random_predict(number: int = 1) -> int:
     """
     count = 0
 
-    while True:
+    #while True:
+    #    count += 1
+    #    predict_number = np.random.randint(1, 101)  # предполагаемое число
+    while number != predict_number:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        #predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number > predict_number:
+            predict_number = predict_number + (predict_number // 2)
+        if number < predict_number:
+                predict_number -= 1 
         if number == predict_number:
             break  # выход из цикла если угадали
     return count
